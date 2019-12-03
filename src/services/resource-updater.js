213785b02c614c9930e8e5651d59f4b993fcd585
Daniel Wallace <daniel@parachut.co>
2019-12-03 18:19:46 -0500

    Update resource-updater.js

--- src/services/resource-updater.js
+++ src/services/resource-updater.js
@@ -1,26 +1,33 @@
-const _ = require('lodash');
-const Interface = require('forest-express');
-const { ErrorHTTP422 } = require('./errors');
-const ResourceGetter = require('./resource-getter');
-const CompositeKeysManager = require('./composite-keys-manager');
-const ResourceFinder = require('./resource-finder');
+const _ = require("lodash");
+const Interface = require("forest-express");
+const { ErrorHTTP422 } = require("./errors");
+const ResourceGetter = require("./resource-getter");
+const CompositeKeysManager = require("./composite-keys-manager");
+const ResourceFinder = require("./resource-finder");
 
 function ResourceUpdater(model, params, newRecord) {
   const schema = Interface.Schemas.schemas[model.name];
 
   this.perform = () => {
-    const compositeKeysManager = new CompositeKeysManager(model, schema, newRecord);
+    const compositeKeysManager = new CompositeKeysManager(
+      model,
+      schema,
+      newRecord
+    );
 
     return new ResourceFinder(model, params)
       .perform()
-      .then((record) => {
+      .then(record => {
         if (record) {
-          _.each(newRecord, (value, attribute) => {
+          _.forOwn(newRecord, (value, attribute) => {
             record[attribute] = value;
           });
 
-          return record.validate()
-            .catch((error) => { throw new ErrorHTTP422(error.message); })
+          return record
+            .validate()
+            .catch(error => {
+              throw new ErrorHTTP422(error.message);
+            })
             .then(() => record.save());
         }
         return null;
@@ -31,7 +38,7 @@ function ResourceUpdater(model, params, newRecord) {
         }
 
         return new ResourceGetter(model, {
-          recordId: params.recordId,
+          recordId: params.recordId
         }).perform();
       });
   };
